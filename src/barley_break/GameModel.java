23b41227c55ff6bf814687dcfb40e8b1a86ac8eb
Роман <bolobolbfmv@gmail.com>
2017-01-10 22:21:20 +0300

    fix generation blocked cell

--- src/barley_break/GameModel.java
+++ src/barley_break/GameModel.java
@@ -35,17 +35,21 @@ public class GameModel {
     private void generateField() {
 
         field().clear();
+
         field().setSize(4, 4);
+
+        int[]  available = {1,4,6,7,10,11,13,16};
+
         size = field().height() * field().width();
         int count = 1;
         int stick = (int) (Math.random() * 16);
-        int block = (int) (Math.random() * 16);
+        int block = (int) (Math.random() * 8);
         for (int row = 1; row <= field().height(); row++) {
             for (int col = 1; col <= field().width(); col++) {
                 if(count == stick) {
                     field().setCell(new Point(col, row), new StickyCell(), String.valueOf(count));
                 }
-                else if (count == block)
+                else if (count == available[block])
                 {
                     field().setCell(new Point(col, row), new BrokenCell(), String.valueOf(count));
                 }
